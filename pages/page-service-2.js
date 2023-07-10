56355f745dee6790a08decb7d38406e2d077cbf9
sesettikalyan <136800341+sesettikalyan@users.noreply.github.com>
2023-07-10 06:48:00 +0000

    Update page-service-2.js

--- pages/page-service-2.js
+++ pages/page-service-2.js
@@ -2,7 +2,6 @@
 import Link from "next/link";
 import Accordion from "../components/elements/Accordion";
 import Layout from "../components/layout/Layout";
-import OfferSlider from "../components/slider/Offer";
 
 
 function Service2() {
@@ -11,6 +10,16 @@ function Service2() {
 
             
                 <section className="section-box">
+                    <div className="back">
+                        <Link href="/" style={{
+                            position:'absolute',
+                            textDecoration:'none',
+                            fontSize:'25px',
+                            right:'25px',
+                            top:'20px'
+                        }}
+                        >Home</Link>
+                    </div>
                     <div className="pt-20 pl-20  d-flex">
                     <h1 className="text-heading-1 text-start color-gray-900"  style={{
                         padding:'20px'
@@ -34,7 +43,7 @@ function Service2() {
                             <p className="text-center fs-4"><strong>Community Partner</strong></p>
                         </div>
                         
-                        <div className="partners3" style={{
+                        <div className="partners2" style={{
                             width:'250px',
                             height:'150px'}}>
                             <img src="/assets/imgs/page/homepage4/img/execution.jpeg" alt="reachx" style={{
@@ -49,21 +58,21 @@ function Service2() {
                             justifyContent:'space-around',
                             marginTop:'5%'
                      }}>
-                        <div className="partners1" style={{
-                            width:'150px',
+                        <div className="partners3" style={{
+                            width:'250px',
                             height:'150px'}}>
                             <img src="/assets/imgs/page/homepage4/img/execution.jpeg" alt="reachx" style={{
                                 width:'150px',
-                                height:'150px'}}/>
+                                height:'150px',marginLeft:'45px'}}/>
                             <p className="text-center fs-4"><strong>Community Partner</strong></p>
                         </div>
                         
-                        <div className="partners3" style={{
-                            width:'150px',
+                        <div className="partners4" style={{
+                            width:'250px',
                             height:'150px'}}>
                             <img src="/assets/imgs/page/homepage4/img/execution.jpeg" alt="reachx" style={{
                                 width:'150px',
-                                height:'150px' }}/>
+                                height:'150px',marginLeft:'45px' }}/>
                             <p className="text-center fs-4"><strong>Technology Partner</strong></p>
                         </div>
                         
