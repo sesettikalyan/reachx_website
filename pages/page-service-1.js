88ab927d1893732fb74e1fae8df17bbeb0bd0e0f
sesettikalyan <136800341+sesettikalyan@users.noreply.github.com>
2023-07-10 08:13:59 +0000

    Update page-service-1.js

--- pages/page-service-1.js
+++ pages/page-service-1.js
@@ -10,65 +10,29 @@ function Service1() {
         <>
 
             
-                <section className="section-box h-100">
+                <section className="people h-100">
                     <div>
-                        <Link href="/" style={{
-                            position:'absolute',
-                            textDecoration:'none',
-                            fontSize:'25px',
-                            right:'25px',
-                            top:'20px'
-                        }}
-                        >Home</Link>
+                        <Link href="/" className="hello">Home</Link>
                     </div>
-                    <div className="key-people  d-flex " style={{
-                        justifyContent:'space-between'
-                    }}>
-                        <div className="head2" 
-                            style={{
-                                marginLeft: '30px',
-                                marginRight: '30px',
-                                margin:'30px'
-                                
-                            }}
-                        >
-                            <h1 className="text-heading-1 text-start color-gray-900 ">KEY <br />PEOPLE</h1>
-                            <div className="director bg-warning "
-                                style={{
-                                width:'400px',
-                                height:'500px',
-                                marginBottom:'-30px'
-                                }}
-                            >
-                                <img className="p-20 rounded-circle " style={{marginLeft:'12%',width:'300px',height:'300px',borderRadius:'300px'}}  src="/assets/imgs/page/homepage4/img/research.jpeg" alt="" />
-                                <h4 className=" text-center text-black"><strong>Mr Dhanesh</strong></h4>
+                    <div className="key-people  d-flex ">
+                        <div className="head2" >
+                            <h1 className="m1 text-heading-1 text-start color-gray-900 ">KEY <br />PEOPLE</h1>
+                            <div className="director ">
+                                <img className="p-20 rounded-circle " src="/assets/imgs/page/homepage4/img/research.jpeg" alt="" />
+                                <h4 className=" text-center text-white"><strong>Mr Dhanesh</strong></h4>
                                 <p className="text-center fs-5 p-10 "><strong>Managing Director</strong></p>
                                 <h6 className="text-center p-10">4+ years of expertise driving transformative solutions in operations, strategy, and innovation across diverse business fields.</h6>
                             </div>
                         </div>
-                            <div className="manager  bg-secondary "
-                                style={{
-                                    width:'400px',
-                                    height:'40%',
-                                }}
-                            >
-                                <img className="p-20 w-5" style={{marginLeft:'12%',width:'300px',height:'300px',borderRadius:'300px'}} src="/assets/imgs/page/homepage4/img/research.jpeg" alt="" />
-                                <h4 className=" text-center text-black"><strong>Mr Prabhakar Rao</strong></h4>
-                                <p className="text-center fs-5 p-10 "><strong>CMO, Client Relation <br />Manager.</strong></p>
-                                <h6 className="text-center p-10">4+ years of expertise <br />in marketing and lead<br /> generation across various <br /> business domains.</h6> 
+                            <div className="manager">
+                                <img className="p-20 w-5" src="/assets/imgs/page/homepage4/img/research.jpeg" alt="" />
+                                <h4 className=" text-center text-white"><strong>Mr Prabhakar Rao</strong></h4>
+                                <p className="text-center text-white fs-5 p-10 "><strong>CMO, Client Relation <br />Manager.</strong></p>
+                                <h6 className="text-center text-white p-10">4+ years of expertise <br />in marketing and lead<br /> generation across various <br /> business domains.</h6> 
                             </div>
-                            <div className="digital k"
-                                style={{
-                                    marginLeft: '30px',
-                                    marginRight: '30px',
-                                    marginTop: '150px',
-                                    width:'400px',
-                                    
-                                    
-                                }}
-                            >
-                                <img className="p-20 rounded-circle " style={{marginLeft:'12%',width:'300px',height:'300px',borderRadius:'300px'}}  src="/assets/imgs/page/homepage4/img/research.jpeg" alt="" />
-                                <h4 className=" text-center text-black"><strong>Mr Suresh kumar</strong></h4>
+                            <div className="digital k">
+                                <img className="p-20 rounded-circle "  src="/assets/imgs/page/homepage4/img/research.jpeg" alt="" />
+                                <h4 className=" text-center  text-white"><strong>Mr Suresh kumar</strong></h4>
                                 <p className="text-center fs-5 p-10 "><strong>Digital Marketing Manager.</strong></p>
                                 <h6 className="text-center p-10">7+ years of expertise in data-driven digital marketing, developing impactful strategies to create a lasting impact in various business domains</h6>
                             </div>
